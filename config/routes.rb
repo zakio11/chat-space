5ac0081a82b8a0bdb5a0e792dd27aa8eb6137e48
zakio <yu.m1wd11@@gmail.com>
2020-04-25 19:30:07 +0900

    flont/create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
+  root "messages#index"
+
+  get 'messages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
