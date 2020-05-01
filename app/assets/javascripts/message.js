d99d3df1f27c8dc12f757a4df58c42ecf61772fa
zakio <yu.m1wd11@@gmail.com>
2020-05-01 20:54:37 +0900

    automatic-update-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,8 +2,8 @@ $(function(){
   function buildHTML(message){
     if ( message.image) {
       var html =
-        `<div class = "chat-main__message__list">
-          <div class = "chat-main__message__list__info">
+        `<div class = "chat-main__message__list" data-message-id=${message.id} >
+          <div class = "chat-main__message__list__info" >
             <div class = "chat-main__message__list__info__talker">
               ${message.user_name}
             </div>
@@ -19,8 +19,8 @@ $(function(){
       return html;
     } else {
       var html =
-        `<div class = "chat-main__message__list">
-        <div class = "chat-main__message__list__info">
+        `<div class = "chat-main__message__list" data-message-id=${message.id}>
+          <div class = "chat-main__message__list__info" >
           <div class = "chat-main__message__list__info__talker">
             ${message.user_name}
           </div>
@@ -55,6 +55,32 @@ $(function(){
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+    });
   });
-  });
+  var reloadMessages = function() {
+    var last_message_id = $('.chat-main__message__list:last').data("message-id");
+    console.log(last_message_id);
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message').append(insertHTML);
+        $('.chat-main__message').animate({ scrollTop: $('.chat-main__message')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+     setInterval(reloadMessages, 7000);
+  }
 });
